4169f651ca63d0facdea5eec9be288425a3b4df7
joao <jvsadesign@gmail.com>
2018-08-20 12:20:14 -0300

    alteracao final

---
+++ script.js
@@ -0,0 +1,32 @@
+let nome = prompt("Olá, qual seu nome?");
+
+let idade = Number(prompt("Qual a sua idade?"));
+
+let salario = Number(prompt("Qual o seu salário liquido?"));
+
+let minemprestimo = 1500
+
+let minidade = 22
+
+let maxidade = 60
+
+let emprest = Number(prompt ("Quanto você quer?"));
+
+let parcelas = Number (prompt("Em quantas parcelas?"));
+
+if (idade >= 22 && (idade <= 60 && salario >= 1500 && (emprest  >= 1000 && (10* salario)))) {
+
+    
+    alert(`Será parcelas ${parcelas} de ${1.6* emprest / parcelas}`);
+}
+
+else { 
+
+    alert(`Empréstimo Reprovado`)
+}
+
+parcelas 
+
+
+
+
